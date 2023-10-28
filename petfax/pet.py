f7997cf7bc030bf83946e060382c3e745dc51550
ACGreve <greve5921@gmail.com>
2023-10-28 13:47:24 -0400

    added new pages and got facts and new to work!

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,6 +8,8 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
 def index(): 
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
 
-
+@bp.route('/show')
+def new():
+    return render_template('pets/show.html')
